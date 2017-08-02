0936a28c676da8f9ae98a7474ae5805c975927c6
Boris Cherepakha <bcherepakha@gmail.com>
2017-08-02 13:34:47 +0300

    initial example

---
+++ Gulpfile.js
@@ -0,0 +1,124 @@
+'use strict';
+
+/* Подключение необходимых плагинов */
+const gulp = require('gulp'),
+    fs = require('fs'),
+    path = require('path'),
+    watch = require('gulp-watch'),
+    rename = require("gulp-rename"),
+    del = require('del'),
+    rigger = require('gulp-rigger'),
+    concat = require('gulp-concat'),
+    streamqueue = require('streamqueue'),
+    less = require('gulp-less'),
+    lessReporter = require('gulp-less-reporter'),
+    csscomb = require('gulp-csscomb'),
+    cleancss = require('gulp-clean-css'),
+    uglify = require('gulp-uglify'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload,
+    runSequence = require('run-sequence').use(gulp),
+    babel = require('gulp-babel'),
+    nunjucksRender = require('gulp-nunjucks-render'),
+    data = require('gulp-data');
+
+/* Конфигурация BrowserSync */
+const config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: false,
+    host: 'localhost',
+    port: 9000,
+    injectChanges: true,
+    logPrefix: "BrowserSync Log"
+};
+
+/* BrowserSync*/
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+gulp.task('js', function () {
+    return gulp.src('src/blocks/**/*.js')
+        .pipe(rigger())
+        .pipe(concat('script.js'))
+        .pipe(babel())
+        .pipe(gulp.dest('build/js/'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js/'))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('less', function() {
+    return gulp.src(['src/base.less', 'src/blocks/**/*.less'])
+        .pipe(less())
+        .on('error', lessReporter)
+        .pipe(csscomb())
+        .pipe(concat('index.css'))
+        .pipe(gulp.dest('build/'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(cleancss())
+        .pipe(gulp.dest('build/'))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('images', function () {
+    return gulp.src('src/blocks/**/img/*.*')
+        .pipe(gulp.dest('build/img/'))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('static', function (cb) {
+    return gulp.src('src/static/**/*')
+        .pipe(gulp.dest('build/'))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('html', function () {
+    return gulp.src(['src/pages/*.njk'])
+        .pipe(data(function(file) {
+            const filePath = path.resolve( 'src/data', path.parse(file.path).name ) + '.json';
+
+            if (fs.existsSync(filePath)) {
+                return JSON.parse(fs.readFileSync(filePath));
+            }
+
+            return {};
+        }))
+        .pipe(nunjucksRender({
+            path: ['./src/blocks/', './src/layouts/'],
+            ext: '.html'
+        }))
+        .pipe(gulp.dest('build/'))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('clean', function (cb) {
+    return del(['build/**/*'], cb);
+});
+
+gulp.task('build', function(cb) {
+    return runSequence('clean', 'static', 'less', 'images', 'js', 'html', cb);
+});
+
+gulp.task('watch', ['webserver'], function() {
+    watch(['src/static/**/*'], function(event, cb) {
+        gulp.start('static');
+    });
+    watch(['src/base.less', 'src/blocks/**/*.less'], function(event, cb) {
+        gulp.start('less');
+    });
+    watch(['src/blocks/**/img/*.*'], function(event, cb) {
+        gulp.start('images');
+    });
+    watch(['src/blocks/**/*.js'], function(event, cb) {
+        gulp.start('js');
+    });
+    watch(['src/pages/*.njk', '.src/blocks/**/*.njk', './src/layouts/*.njk'], function(event, cb) {
+        gulp.start('html');
+    });
+});
+
+gulp.task('default', ['watch'], function() {});
--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# gulp-default
-default gulp project
+# Базовый шаблон проекта на Gulp
+
+Пример шаблона для начала работ над frontend проектами
+
+## Перед началом работы
+
+Для работы шаблона необходимо установить `nodejs` (вместе с `npm`)
+
+- [Установка Nodejs](https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager "Installing Node.js via package manager")
+
+## Установка шаблона
+
+``` sh
+$ git clone https://github.com/bcherepakha/gulp-default.git project-name
+$ cd project-name
+$ npm install
+```
+
+По окончанию выполнения будут установлены все необходимые пакеты.
+
+### Задачи Gulp
+
+ - `$ gulp html` - сборка страниц
+ - `$ gulp js` - сборка JS
+ - `$ gulp less` - компилиция LESS
+ - `$ gulp clean` - очистка каталога `build/`
+ - `$ gulp webserver` - запуск локального веб-сервера для livereload
+ - `$ gulp build` - полная сборка проекта
+ - `$ gulp watch` - запуск задачи `webserver` и отслеживания изменений
+ - `$ gulp default` - запуск задачи `watch`
+
+## Общая концепция
+
+- `src/` - каталог для размещения рабочих файлов (html, less, js, изображения)
+- `src/blocks` - блоки из которых собирается проект
+- `src/layouts` - layouts страниц
+- `src/pages` - страницы
+- `build/` - каталог для размещения готовой верстки
+
+Вся работа осуществляется в каталоге `src/`.
+
+### Livereload и синхронизация между браузерами
+
+Задача `$ gulp webserver`
+
+При выполнении задачи запускается локальный веб-сервер BrowserSync и открыватся index.html проекта.  
+
+[Подробнее о BrowserSync](http://www.browsersync.io/ "Подробнее о BrowserSync")  
+
+Сервер использует каталог `build/` в качестве корня проекта.
+
+### Отслеживание изменений
+
+Задача `$ gulp watch`
+
+При запуске сначала выполняется задача `$ gulp webserver`, затем при изменении или добавлении в каталоге `src/` каких  
+либо файлов, автоматически запускается соответсвующая задача по их обработке.
